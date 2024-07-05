5b416b6fe57ec4f4a79e9020cb9ea2e026369638
Miroslav Jonas <missing.manual@gmail.com>
2024-07-05 15:25:46 +0200

    feat: move fe to subfolder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [sveltekit(), nxViteTsPaths()],
-  cacheDir: './node_modules/.vite/frontend',
+  cacheDir: '../node_modules/.vite/frontend',
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
@@ -13,15 +13,11 @@ export default defineConfig(async () => ({
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
-    strictPort: true,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/app/**"],
-    },
+    strictPort: true
   },
 
   build: {
-    outDir: './dist/frontend',
+    outDir: '../dist/frontend',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
